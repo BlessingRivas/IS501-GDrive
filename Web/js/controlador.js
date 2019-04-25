169c4203cbb090d46f410cb6791a08c247752499
Ariadna <ariadna.perez97@yahoo.com>
2019-04-24 21:25:55 -0600

    login

--- Web/js/controlador.js
+++ Web/js/controlador.js
@@ -1,96 +1,36 @@
-var actualPosition = 0;
-
-$(document).on('scroll',function(){
-    var topVentana = $(document).scrollTop();
-    var bottomVentana = window.innerHeight + topVentana;
-    var logoPosition = $("#animate-logo").offset().top;
-    var bottomLogo = logoPosition + $("#animate-logo").height();
-
-    if(topVentana > logoPosition - 300){
-
-        if($("#animate-logo").css("display") != "none"){
-            $("#animate-logo").css("animation","fadeOutLogo 2s forwards");            
-            $("#animate-file-types").css("animation","fadeInFileTypes 2s ease 500ms forwards");            
-        }        
+/*$(".login-container").on("click",function(){    
+    console.log(FocusEvent.prototype);
+    if($(".login-field-title").css("display") == "block"){
+        $(".login-field-title").css("display","none");
+        $("#txt-email").attr("placeholder","Correo electrónico o teléfono");
+        $(".login-field-email").css("border","1px solid lightgray");
     }
+});*/
 
-    //var divBottom = $("#files-everywhere").height() + $("#files-everywhere").offset().top;
-    var divBottom = $("#save-any-file").offset().top;    
+var placeholder="";
 
-    if(topVentana > divBottom - 50){
-        $("#changing-link").css("display","block");
-        $("#changing-link").css("animation","rollInLink 400ms forwards");
-        $(".navbar-brand").css("color","black");
-        $("#brand-logo").attr("src","img/brand-logo-2.png");
-        $("#main-nav").css("background-color", "white");
-        $(".nav-link").css("color","#555555");
-    }
-    else{        
-        $("#changing-link").css("display","none");
-        $("#changing-link").css("animation","none");
-        $(".navbar-brand").css("color","white");
-        $("#brand-logo").attr("src","img/brand-logo-1.png");
-        $("#main-nav").css("background-color","transparent");
-        $(".nav-link").css("color","white");
-    }
-    
-    var newPosition = $(document).scrollTop();
-
-    var opacity = parseFloat($("#fading-div").css("opacity"));
-
-    var topSection1 = $("#surf-guy").offset().top;
-    var bottomSection1 = $("#surf-guy").offset().top + $("#surf-guy").height();
-    var actualMargin1 = parseInt($("#surf-guy img").css("margin-top"));
-    var topSection2 = $("#sharing-cup").offset().top;
-    var bottomSection2 = $("#sharing-cup").offset().top + $("#sharing-cup").height();
-    var actualMargin2 = parseInt($("#sharing-cup img").css("margin-top"));
-    var topSection3 = $("#dropped-stuff").offset().top;
-    var bottomSection3 = $("#dropped-stuff").offset().top + $("#dropped-stuff").height();
-    var actualMargin3 = parseInt($("#dropped-stuff img").css("margin-top"));
-
-    if(newPosition > actualPosition){
-        if(newPosition > $("#fading-div").offset().top && newPosition < divBottom){            
-            $("#fading-div").css("opacity",(opacity - 0.01) + "");
-        }
-        
-        if(newPosition > (topSection1 - 100) && newPosition < bottomSection1 + 50){            
-            $("#surf-guy img").css("margin-top",(actualMargin1 - 1) + "px");
-        }
-
-        if(newPosition > (topSection2 - 100) && newPosition < bottomSection2 + 50){            
-            $("#sharing-cup img").css("margin-top",(actualMargin2 - 1) + "px");
-        }
-
-        if(newPosition > (topSection3 - 100) && newPosition < bottomSection3 + 50){            
-            $("#dropped-stuff img").css("margin-top",(actualMargin3 - 1) + "px");
-        }
-
-    }else{
-        if(newPosition < $("#fading-div").offset().top){
-            $("#fading-div").css("opacity","1");
-        }
-        else if(newPosition > $("#fading-div").offset().top && newPosition < divBottom){            
-            $("#fading-div").css("opacity",(opacity + 0.01) + "");
-        }
-
-        if(newPosition > (topSection1 - 50) && newPosition < (bottomSection1 + 100)){
-            $("#surf-guy img").css("margin-top",(actualMargin1 + 1) + "px");
-        }
-
-        if(newPosition > (topSection2 - 50) && newPosition < (bottomSection2 + 100)){
-            $("#sharing-cup img").css("margin-top",(actualMargin2 + 1) + "px");
-        }
-
-        if(newPosition > (topSection3 - 50) && newPosition < (bottomSection3 + 100)){
-            $("#dropped-stuff img").css("margin-top",(actualMargin3 + 1) + "px");
-        }
-    }
-
-    actualPosition = newPosition;
-
+$("#txt-email,#txt-password").on("focus",function(){
+    $(".login-field-title").css("display","block");
+    placeholder = $(this).attr("placeholder");
+    $(this).attr("placeholder","");
+    $(".login-field-email").css("border","2px solid blue");    
 });
 
-$("#scroll-arrow").on("click",function(){
-    var scrollTo = window.innerHeight;
-    $('html, body').animate({scrollTop: scrollTo},1000);    
+$("#txt-email,#txt-password").on("blur",function(){    
+    $(".login-field-title").css("display","none");    
+    $(this).attr("placeholder",placeholder);
+    $(".login-field-email").css("border","1px solid lightgray");
+});
+
+$("#sh-password").click(function(){
+    if($("#txt-password").attr("type") == "text"){
+        $("#txt-password").attr("type","password");
+        $(this).html('<i class="far fa-eye">');
+        $("#txt-password").focus();
+    }
+    else{
+        $("#txt-password").attr("type","text");
+        $(this).html('<i class="far fa-eye-slash">');
+        $("#txt-password").focus();
+    }
 });
